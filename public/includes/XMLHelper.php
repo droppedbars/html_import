aebeb32cbdc31475c7b28594ec0a7efabb92d0be
Patrick Mauro <patrick@mauro.ca>
2014-10-22 21:40:13 -0700

    add support for imports from the web
    tweaks to support importing using a URL instead of a directory path.

--- public/includes/XMLHelper.php
+++ public/includes/XMLHelper.php
@@ -31,8 +31,30 @@ class XMLHelper {
 		return $simple_xml;
 	}
 
+	// TODO: find an appropriate place for this
+	// lifted from: http://php.net/manual/en/function.file-exists.php
+	public static function url_exists($url) {
+		// Version 4.x supported
+		$handle   = curl_init($url);
+		if (false === $handle)
+		{
+			return false;
+		}
+		curl_setopt($handle, CURLOPT_HEADER, false);
+		curl_setopt($handle, CURLOPT_FAILONERROR, true);  // this works
+		curl_setopt($handle, CURLOPT_HTTPHEADER, Array("User-Agent: Mozilla/5.0 (Windows; U; Windows NT 5.1; en-US; rv:1.8.1.15) Gecko/20080623 Firefox/2.0.0.15") ); // request as if Firefox
+		curl_setopt($handle, CURLOPT_NOBODY, true);
+		curl_setopt($handle, CURLOPT_RETURNTRANSFER, false);
+		$connectable = curl_exec($handle);
+		curl_close($handle);
+		return $connectable;
+	}
+
+	// TODO: this should be beefed up to actually validate that it's an XML
 	public static function valid_xml_file( $xml_path ) {
-		if ( file_exists( $xml_path ) ) {
+		if (filter_var($xml_path, FILTER_VALIDATE_URL)) { // if URL
+			return self::url_exists( $xml_path );
+		} else if ( file_exists( $xml_path ) ) {
 			return true;
 		}
 
